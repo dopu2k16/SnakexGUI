17d428f58117dbd8cada5c7c1d90dbc6a6d891aa
dopu2k16 <mitodru.niyogi@gmail.com>
2016-02-01 02:32:46 +0530

    first commit.

---
+++ src/com/game/snake/Main.java
@@ -0,0 +1,13 @@
+package com.game.snake;
+/**
+*
+* @author Mitodru
+*/
+public class Main {
+   /**
+    * @param args the command line arguments
+    */
+   public static void main(String[] args) {
+       new Snake();
+   }
+}
